66b766a15c1323ff69242f7baae2e3d0966c84fb
Jefferson <jeffarrubla@gmail.com>
2021-07-07 14:31:07 +0000

    Added Cell class

--- minesweeper/views.py
+++ minesweeper/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+VALUE_MINE = '*' 
+
+class Cell:
+	"""
+		Individual cell of the minefield
+
+		When created it's not flagged and not visible.
+	"""
+
+	def __init__(self, value=0, flagged=False, visible=False):
+		self._value = value
+        self.flagged = flagged
+        self.visible = visible
+
+	def is_mine(self):
+        """Check if the cell is a mine."""
+        return self.value == VALUE_MINE
+
+	def __str__(self):        
+        if not self.visible:
+            return ' '
+        if self.flagged:
+            return 'f'
+        if self.is_mine():
+            return '*'
+        return str(self.value)
